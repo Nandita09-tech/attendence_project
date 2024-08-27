611723765d2555942b88fa313fad6696536eac2a
Nandita09-tech <nanditarajput147@gmail.com>
2024-08-27 10:07:32 -0600

    mongoDB

---
+++ routes/authRoutes.js
@@ -0,0 +1,17 @@
+
+const express = require('express');
+const authController = require('../controllers/authController');
+const router = express.Router();
+
+
+
+//views
+router.get('/login', (req, res) =>{ res.render('login');});
+router.get('/register', (req, res) =>{res.render('register');});
+
+//controller actions
+router.post('/register', authController.register);
+
+
+
+module.exports = router;
